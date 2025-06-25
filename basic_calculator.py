43757b92b85a4e74134797e939202535fd083a04
Nirvaan-12 <anu.shanagonda@gmail.com>
2025-06-25 22:33:59 +0530

    Initial commit:added basic python files

---
+++ basic_calculator.py
@@ -0,0 +1,14 @@
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+operation=input("Choose operation(+, -, *, /):")
+
+if operation == '+':
+    print(a+b)
+elif operation == '-':
+    print(a-b)
+elif opration== '*':
+    print(a*b)
+elif opration== '/':
+    print(a/b)
+else:
+    print("Invalid operation")
